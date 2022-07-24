133a5428f5bf5865431dfb3309bbb8eb870b9b3c
Angel Lammenda <angellammenda@hotmail.com>
2022-07-24 15:11:01 -0300

    trabajando sobre la entrega final

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
 import ItemDetailContainer from './components/ItemDetailContainer';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
-// import Cart from './components/cart/Cart';
+import Cart from './components/cart/Cart';
 
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
     <Route path='/' element={<ItemListContainer /> }/>
     <Route path='/category/:name' element={<ItemListContainer /> }/>
     <Route path='/item/:id' element={<ItemDetailContainer /> }/>
-    {/* <Route path='/cart' element={<Cart /> }/> */}
+    <Route path='/cart' element={<Cart /> }/>
     <Route
              path="*"
              element={
